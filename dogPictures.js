43908fc75c7a121fd2fb94f21cc244665cbe4ba2
@nain333 <hnain70@gmail.com>
2023-01-23 15:45:38 +0530

    edit css

--- dogPictures.js
+++ dogPictures.js
@@ -2,11 +2,13 @@ function fetchDogPics(){
     var xhrRequest= new XMLHttpRequest;
     xhrRequest.onload=function(){
         $('#fetcher').prop('disabled',true);
+        $('#fetcher').html('Loading...');
         console.log(xhrRequest.response);
         var response=JSON.parse(xhrRequest.response);
         var imageURL=response.message;
         $('#dogImage').attr('src',imageURL);
-        $('fetcher').prop('disabled',false);
+        $('#fetcher').prop('disabled',false);
+        $('#fetcher').html('Fetch');
 
 
     }
